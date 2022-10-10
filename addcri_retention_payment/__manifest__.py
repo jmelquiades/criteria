a8221bb3671931ca7b7f21206d2cfdd98434133b
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2022-10-10 18:01:57 -0500

    retencion: nuevos campos en partner y en la configuracion

---
+++ addcri_retention_payment/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Retención',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': 'Jhon Jairo Rojas Ortiz',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'views/res_partner.xml',
+        'views/res_config_settings.xml'
+    ],
+    'demo': [
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {
+
+    }
+}
---
+++ addcri_retention_payment/models/res_partner.py
@@ -0,0 +1,18 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    _description = 'Res Partner'
+
+    is_retention = fields.Boolean('Is retention', default=False)
+    retention_percentage = fields.Selection([
+        ('3', '3%'),
+        ('6', '6%')
+    ], string='Retention Percentage')
+
+    @api.constrains('is_retention', 'retention_percentage')
+    def _constrains_is_retention_retention_percentage(self):
+        if self.is_retention and not self.retention_percentage:
+            raise UserError(_('You must select the retention percentage.'))
