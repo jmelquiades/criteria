6f71cc031f79698f67ff72f523700a40f772c9cf
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2022-10-11 18:00:31 -0500

    tabla 10 sunat

---
+++ addcri_retention_payment/models/account_bank_statement_line.py
@@ -0,0 +1,10 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class AccountBankStatementLine(models.Model):
+    _inherit = 'account.bank.statement.line'
+    _description = 'Account Bank Statement Line'
+
+    l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type', string='Tipo de documento')
+    l10n_latam_table_10_id = fields.Many2one('l10n_latam.table.10', string='Tabla 10')
