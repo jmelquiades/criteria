6ace7b3a8112aa25dcc2d2bef59aad4678a0e353
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2022-10-13 22:08:34 -0500

    tipo de cambio: test de herencia de funciones

---
+++ addcri_exchange_rate_purchase/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Tipo de cambio compra',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': 'Jhon Jairo Rojas Ortiz',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'currency_rate_live'
+    ],
+    'data': [
+        'views/res_currency.xml'
+    ],
+    'demo': [
+
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {
+
+    }
+}
