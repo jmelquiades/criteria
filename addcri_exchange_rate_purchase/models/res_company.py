e0eb9ba9fa2def7c2882e07493e6d77b12f28990
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2022-10-15 20:46:05 -0500

    _logger _parse_bcrp_update_purchase_data dates

--- addcri_exchange_rate_purchase/models/res_company.py
+++ addcri_exchange_rate_purchase/models/res_company.py
@@ -69,6 +69,7 @@ class ResCompany(models.Model):
                 continue
             ########
             dates = self.env['res.currency'].search([('name', '=', currency_odoo_code)]).rate_ids.filtered(lambda r: r.purchase_rate in (False, 1)).mapped('name')
+            _logger.info(f'_parse_bcrp_update_purchase_data dates {dates}')
             for date_pe in dates:
                 second_pe_str = date_pe.strftime(bcrp_date_format_url)
                 data = {
