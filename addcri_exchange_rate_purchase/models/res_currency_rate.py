b6cebce20103b540afef395945b23c99aae1454d
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2022-10-26 18:39:20 -0500

    correnccion en _execute_compute_exchange_rate_moves y se quita por valor por defecto en tipo de cambio

--- addcri_exchange_rate_purchase/models/res_currency_rate.py
+++ addcri_exchange_rate_purchase/models/res_currency_rate.py
@@ -114,8 +114,8 @@ class ResCurrencyRate(models.Model):
 
     def _execute_compute_exchange_rate_moves(self, vals):
         AccountMove = self.env['account.move']
-        company = vals.get('company_id', self.company_id)
-        currency = vals.get('currency_id', self.currency_id)
+        company = vals.get('company_id', self.company_id.id)
+        currency = vals.get('currency_id', self.currency_id.id)
         date = vals.get('name', self.name)
 
         if type(date) == str:
@@ -125,9 +125,9 @@ class ResCurrencyRate(models.Model):
             date_str = date.strftime('%Y-%m-%d')
             date = date
 
-        domain = [('date', '>=', date_str), ('company_id', '=', company.id), ('currency_id', '=', currency.id)]
+        domain = [('date', '>=', date_str), ('company_id', '=', company), ('currency_id', '=', currency)]
 
-        next_dates = currency.rate_ids.filtered(lambda cr: cr.name > date).sorted(lambda d: d.name)
+        next_dates = self.env['res.currency'].browse(currency).rate_ids.filtered(lambda cr: cr.name > date).sorted(lambda d: d.name)
         next_date = next_dates[0].name if next_dates else False
 
         if next_date:
