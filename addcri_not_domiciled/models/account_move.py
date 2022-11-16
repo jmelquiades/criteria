0998594cebc07f88b3106895a95a9dba33c2ec24
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2022-11-16 15:46:05 -0500

    [change] valor de select descubierto en el otro requerimiento

--- addcri_not_domiciled/models/account_move.py
+++ addcri_not_domiciled/models/account_move.py
@@ -9,6 +9,6 @@ class AccountMove(models.Model):
     sunat_table_31_id = fields.Many2one('sunat.table.31', string='Tipo de renta')
     not_domiciled_purchase_move_period = fields.Selection([
         ('0', 'Anotación optativa sin efecto en el IGV corresponde al periodo.'),
-        ('1', 'Ajuste o rectificación en la anotación de la información de una operación registrada en un periodo anterior.')
-    ], string='Estado de factura de compra no domiciliado', default='1')
+        ('9', 'Ajuste o rectificación en la anotación de la información de una operación registrada en un periodo anterior.')
+    ], string='Estado de factura de compra no domiciliado')
     not_domiciled = fields.Boolean('Es no domiciliado?', related='partner_id.not_domiciled')
