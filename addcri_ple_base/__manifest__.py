ad910962896a66682020f30b785867a0d9387645
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2022-11-16 11:29:28 -0500

    [delete] eliminacion de is_nodomicilied en contactos

--- addcri_ple_base/__manifest__.py
+++ addcri_ple_base/__manifest__.py
@@ -14,11 +14,9 @@
         'security/ir.model.access.csv',
         'views/account_journal.xml',
         'views/account_move.xml',
-        # 'views/account_move_line.xml',
         'views/account_retention.xml',
         'views/code_customs.xml',
         'views/res_company.xml',
-        'views/res_partner.xml',
         "views/ple_base.xml"
     ],
     'demo': [
