a23a980bee22f921dfedbcfe9e84057910ca8850
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2022-11-18 00:10:57 -0500

    [change] test : falta que cambie tras cambiar el valor del monto total de detraccion

--- addcri_detraction_accounting_entry/models/account_move.py
+++ addcri_detraction_accounting_entry/models/account_move.py
@@ -1,4 +1,5 @@
 from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class AccountMove(models.Model):
@@ -7,24 +8,33 @@ class AccountMove(models.Model):
 
     def _recompute_dynamic_lines(self, recompute_all_taxes=False, recompute_tax_base_amount=False):
         super(AccountMove, self)._recompute_dynamic_lines(recompute_all_taxes, recompute_tax_base_amount)
-        merc = self.line_ids.filtered(lambda line: not line.exclude_from_invoice_tab)
-        # self._inverse_amount_total()
-        balance = 20
-        values = {
-            'account_id': self.company_id.detraction_outbound_account_id.id,
-            'balance': balance,
-            'debit': balance > 0.0 and balance or 0.0,
-            'credit': balance < 0.0 and -balance or 0.0,
-            'exclude_from_invoice_tab': True
-        }
-        self.line_ids = [(0, 0, values)]
-        if merc:
-            m = merc[0]
-        # for m in merc:
-            balance = m.balance
-            m.update(
-                {
-                    'debit': balance > 0.0 and balance - 20 or 0.0,
-                    'credit': balance < 0.0 and -balance + 20 or 0.0
+        if not self.company_id.detraction_outbound_account_id:
+            raise UserError('Configurar cuenta para pago de detracciones.')
+
+        detraction_outbound_account = self.company_id.detraction_outbound_account_id
+        if self.l10n_pe_dte_is_detraction and self.state == 'draft':
+            merc = self.line_ids.filtered(lambda line: line.exclude_from_invoice_tab and line.account_id == detraction_outbound_account)
+
+            if len(merc) > 1:
+                raise UserError('Hay más de un apunte contable con la cuenta de pago de detracciones.')
+
+            balance = self.l10n_pe_dte_detraction_amount
+            if not merc:
+                values = {
+                    'account_id': detraction_outbound_account.id,
+                    'balance': balance,
+                    'debit': 0.0,
+                    'credit': balance,
+                    'exclude_from_invoice_tab': True,
+                    'move_id': self.id
                 }
-            )
+                self.env['account.move.line'].new(values)
+                line_credit = self.line_ids.filtered(lambda line: line.exclude_from_invoice_tab and line.account_id != detraction_outbound_account and line.credit > 0)
+                line_credit.credit -= balance
+            else:
+                merc.balance = balance
+                merc.credit = balance
+
+    # @api.onchange('l10n_pe_dte_detraction_amount')
+    # def _onchange_l10n_pe_dte_detraction_amount(self):
+    #     self._recompute_dynamic_lines()
--- addcri_detraction_accounting_entry/models/res_company.py
+++ addcri_detraction_accounting_entry/models/res_company.py
@@ -5,5 +5,5 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
     _description = 'Res Company'
 
-    detraction_inbound_account_id = fields.Many2one('account.account', string="Cuenta por pagar de detracciones", copy=True, groups="account.group_account_manager")
-    detraction_outbound_account_id = fields.Many2one('account.account', string="Cuenta por cobrar de detracciones", copy=True, groups="account.group_account_manager")
+    # detraction_inbound_account_id = fields.Many2one('account.account', string="Cuenta por cobrar de detracciones", copy=True, groups="account.group_account_manager")
+    detraction_outbound_account_id = fields.Many2one('account.account', string="Cuenta por pagar de detracciones", copy=True, groups="account.group_account_manager")
