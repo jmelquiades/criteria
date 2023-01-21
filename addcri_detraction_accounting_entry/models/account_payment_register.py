7c710ea9123077684792abc4816a0ba8771c76a4
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2023-01-21 10:36:15 -0500

    se sobreescribe metodo para que no llegue simplemente el primer apunte contable, sino el primero que sea igual al de detracciones cuando el metodo de pago es 'Detracciones'

--- addcri_detraction_accounting_entry/models/account_payment_register.py
+++ addcri_detraction_accounting_entry/models/account_payment_register.py
@@ -49,3 +49,51 @@ class AccountPaymentRegister(models.TransientModel):
             )
         return payment_vals
 
+    def _create_payments(self):
+        self.ensure_one()
+        batches = self._get_batches()
+        edit_mode = self.can_edit_wizard and (len(batches[0]['lines']) == 1 or self.group_payment)
+        to_process = []
+
+        if edit_mode:
+            payment_vals = self._create_payment_vals_from_wizard()
+            data = {
+                'create_vals': payment_vals,
+                'batch': batches[0],
+            }
+            if self.payment_method_line_id.name == 'Detracciones':
+                data.update({
+                    'to_reconcile': sorted(batches, key=lambda r: self.company_id.detraction_outbound_account_id in r.get('lines', []).mapped('account_id'), reverse=True)[0]['lines']
+                })
+            else:
+                data.update({
+                    'to_reconcile': batches[0]['lines'],
+                })
+            to_process.append(data)
+        else:
+            # Don't group payments: Create one batch per move.
+            if not self.group_payment:
+                new_batches = []
+                for batch_result in batches:
+                    for line in batch_result['lines']:
+                        new_batches.append({
+                            **batch_result,
+                            'payment_values': {
+                                **batch_result['payment_values'],
+                                'payment_type': 'inbound' if line.balance > 0 else 'outbound'
+                            },
+                            'lines': line,
+                        })
+                batches = new_batches
+
+            for batch_result in batches:
+                to_process.append({
+                    'create_vals': self._create_payment_vals_from_batch(batch_result),
+                    'to_reconcile': batch_result['lines'],
+                    'batch': batch_result,
+                })
+
+        payments = self._init_payments(to_process, edit_mode=edit_mode)
+        self._post_payments(to_process, edit_mode=edit_mode)
+        self._reconcile_payments(to_process, edit_mode=edit_mode)
+        return payments
