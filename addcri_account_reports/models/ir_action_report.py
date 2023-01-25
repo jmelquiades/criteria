6909a0f6ef857d24ac7f01309cd409414507cc3a
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2023-01-25 13:01:55 -0500

    se modifica wizard para que en titulo tenga el nombre del informe y lo mismo con el pdf descargado: Libro diario

---
+++ addcri_account_reports/models/ir_action_report.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models, tools
+class IrActionsReport(models.Model):
+    _inherit = 'ir.actions.report'
+    _description = 'Report Action'
+
+    def report_action(self, docids, data=None, config=True):
+        """Return an action of type ir.actions.report.
+
+        :param docids: id/ids/browse record of the records to print (if not used, pass an empty list)
+        :param data:
+        :param bool config:
+        :rtype: bytes
+        """
+        report_action = super(IrActionsReport, self).report_action(docids=docids, data=data, config=config)
+        discard_logo_check = self.env.context.get('discard_logo_check')
+        if not(self.env.is_admin() and not self.env.company.external_report_layout_id and config and not discard_logo_check) and data.get('report_name', False):
+            self.name = data.get('report_name')
+
+        return report_action
--- addcri_account_reports/wizards/account_print_journal.py
+++ addcri_account_reports/wizards/account_print_journal.py
@@ -1,12 +1,13 @@
 
 from odoo import fields, models
-from dateutil.relativedelta import relativedelta
-
+# from dateutil.relativedelta import relativedelta
+from odoo.tools.misc import get_lang
 class AccountPrintJournal(models.TransientModel):
     _inherit = "account.print.journal"
     _description = "Account Print Journal"
 
     only_form_l10n_pe = fields.Boolean('Solo se muestran en vista formulario de vistas de l10n_pe')
+    report_name = fields.Char('Nombre del reporte')
 
     def l10n_report(self, journals=False):
         date = fields.Date.today()
@@ -14,20 +15,22 @@ class AccountPrintJournal(models.TransientModel):
         # date_to = (date + relativedelta(months=1)).replace(day=1)
         journal_ids = self.journal_ids.search([]).ids
         only_form_l10n_pe = True
+        report_name = 'Libro diario'
         record = {
             'target_move': 'posted',
             'sort_selection': 'move_name',
             # 'date_from': date_from,
             # 'date_to': date_to,
             'only_form_l10n_pe': only_form_l10n_pe,
-            'journal_ids': [(6, 0, journal_ids)]
+            'journal_ids': [(6, 0, journal_ids)],
+            'report_name': report_name
         }
         journal_report = self.create(record)
 
         view_id = self.env.ref('account_reports.account_report_print_journal_view')
         
         open_view = {
-            'name': 'Libro diario',
+            'name': report_name,
             'view_type': 'form',
             'view_mode': 'form',
             'res_model': 'account.print.journal',
@@ -38,3 +41,9 @@ class AccountPrintJournal(models.TransientModel):
         }
 
         return open_view
+    
+    def pre_print_report(self, data):
+        data = super(AccountPrintJournal, self).pre_print_report(data)
+        if self.report_name:
+            data['report_name'] = self.report_name
+        return data
