0e2cae5851d209e9d944b9d9967e8089ac9ac701
jmelquiades <75193391+jmelquiades@users.noreply.github.com>
2023-03-14 17:05:10 -0400

    Act. Localización Gastos

--- addcri_hr_expense_type_document/models/hr_expense.py
+++ addcri_hr_expense_type_document/models/hr_expense.py
@@ -25,7 +25,8 @@ class HrExpense(models.Model):
                 igv=self.igv
                 rc=self.rc
                 base=total_pagar - igv -rc
-                porcentaje_igv=round((igv*100)//base,0)
+                valor=(igv*100)/float(base)
+                porcentaje_igv=round(valor,0)
                 obj_taxes = self.env['account.tax'].search([('amount','=',porcentaje_igv),(('type_tax_use','=','purchase')),(('price_include','=',True))])
                 if obj_taxes:
                     print(len(self.tax_ids.ids))
@@ -45,8 +46,9 @@ class HrExpense(models.Model):
                 total_pagar=self.total_amount
                 igv=self.igv
                 rc=self.rc
-                base=total_pagar - igv -rc
-                porcentaje_igv=round((igv*100)//base,0)
+                base=total_pagar - igv -rc 
+                valor=(igv*100)/float(base)
+                porcentaje_igv=round(valor,0)
                 obj_taxes = self.env['account.tax'].search([('amount','=',porcentaje_igv),(('type_tax_use','=','purchase')),(('price_include','=',True))])
                 if obj_taxes:
                     self.tax_ids=obj_taxes
@@ -166,58 +168,58 @@ class HrExpense(models.Model):
                 move_line_values.append(move_line_tax_values)
             
             
-        # RECARGO POR CONSUMO
-        if  total_rc > 0:
-            total_amount -= total_rc
-            total_amount_currency -= total_rc
-            move_line_rc = {
-                'name': 'Recargo al Consumo',
-                'quantity': 1,
-                'debit': total_rc,
-                'credit': 0,
-                'amount_currency': total_rc,
-                'account_id': account_src.id,
-                'tax_base_amount': base_amount,
-                'expense_id': expense.id,
-                'partner_id': partner_id,
-                'currency_id': expense.currency_id.id
-            }
-            print('----:',move_line_rc)
-            move_line_values.append(move_line_rc)
+            # RECARGO POR CONSUMO
+            if  total_rc > 0:
+                total_amount -= total_rc
+                total_amount_currency -= total_rc
+                move_line_rc = {
+                    'name': 'Recargo al Consumo',
+                    'quantity': 1,
+                    'debit': total_rc,
+                    'credit': 0,
+                    'amount_currency': total_rc,
+                    'account_id': account_src.id,
+                    'tax_base_amount': base_amount,
+                    'expense_id': expense.id,
+                    'partner_id': partner_id,
+                    'currency_id': expense.currency_id.id
+                }
+                print('----:',move_line_rc)
+                move_line_values.append(move_line_rc)
 
-        # REDONDEO
-        if  total_redondeo > 0:
-            total_amount -=  total_redondeo
-            total_amount_currency -=  total_redondeo
-            move_line_redondeo = {
-                'name': 'Dscto Redondeo',
-                'quantity': 1,
-                'debit': total_redondeo,
-                'credit': 0,
-                'amount_currency': total_redondeo,
-                'account_id': account_src.id,
-                'tax_base_amount': base_amount,
-                'expense_id': expense.id,
-                'partner_id': partner_id,
-                'currency_id': expense.currency_id.id
-            }
-            print('----:',move_line_redondeo)
-            move_line_values.append(move_line_redondeo)
+            # REDONDEO
+            if  total_redondeo > 0:
+                total_amount -=  total_redondeo
+                total_amount_currency -=  total_redondeo
+                move_line_redondeo = {
+                    'name': 'Dscto Redondeo',
+                    'quantity': 1,
+                    'debit': total_redondeo,
+                    'credit': 0,
+                    'amount_currency': total_redondeo,
+                    'account_id': account_src.id,
+                    'tax_base_amount': base_amount,
+                    'expense_id': expense.id,
+                    'partner_id': partner_id,
+                    'currency_id': expense.currency_id.id
+                }
+                print('----:',move_line_redondeo)
+                move_line_values.append(move_line_redondeo)
 
             # destination move line
 
             move_line_dst = {
-                'name': move_line_name,
-                'debit': total_amount > 0 and total_amount,
-                'credit': total_amount < 0 and -total_amount,
-                'account_id': account_dst,
-                'date_maturity': account_date,
-                'amount_currency': total_amount_currency,
-                'currency_id': expense.currency_id.id,
-                'expense_id': expense.id,
-                'partner_id': partner_id,
-                'exclude_from_invoice_tab': True,
-            }
+                    'name': move_line_name,
+                    'debit': total_amount > 0 and total_amount,
+                    'credit': total_amount < 0 and -total_amount,
+                    'account_id': account_dst,
+                    'date_maturity': account_date,
+                    'amount_currency': total_amount_currency,
+                    'currency_id': expense.currency_id.id,
+                    'expense_id': expense.id,
+                    'partner_id': partner_id,
+                    'exclude_from_invoice_tab': True,
+                }
             move_line_values.append(move_line_dst)
             
             move_line_values_by_expense[expense.id] = move_line_values
@@ -239,3 +241,51 @@ class HrExpense(models.Model):
                                 'sequence_id': obj_sequence.id
                             }
                             self.env['ir.sequence.date_range'].sudo().create(values)
+
+    def create_expense_from_attachments(self, attachment_ids=None, view_type='tree'):
+            ''' Create the expenses from files.
+            :return: An action redirecting to hr.expense tree view.
+            '''
+            if attachment_ids is None:
+                attachment_ids = []
+            attachments = self.env['ir.attachment'].browse(attachment_ids)
+            if not attachments:
+                raise UserError(_("No attachment was provided"))
+            expenses = self.env['hr.expense']
+
+            if any(attachment.res_id or attachment.res_model != 'hr.expense' for attachment in attachments):
+                raise UserError(_("Invalid attachments!"))
+
+            product = self.env['product.product'].search([('can_be_expensed', '=', True)])
+            if product:
+                product = product.filtered(lambda p: p.default_code == "EXP_GEN") or product[0]
+            else:
+                raise UserError(_("You need to have at least one category that can be expensed in your database to proceed!"))
+
+            typedocument = self.env['l10n_latam.document.type'].search([('use_in_expense', '=', True)])
+
+            if typedocument:
+                 id_typedocument =typedocument.ids[0]
+            else:
+                 id_typedocument =False
+            for attachment in attachments:
+                
+                expense = self.env['hr.expense'].create({
+                    'name': attachment.name.split('.')[0],
+                    'unit_amount': 0,
+                    'product_id': product.id,
+                    'l10n_latam_document_type_id':id_typedocument
+                })
+                attachment.write({
+                    'res_model': 'hr.expense',
+                    'res_id': expense.id,
+                })
+                attachment.register_as_main_attachment()
+                expenses += expense
+            return {
+                'name': _('Generated Expenses'),
+                'res_model': 'hr.expense',
+                'type': 'ir.actions.act_window',
+                'views': [[False, view_type], [False, "form"]],
+                'context': {'search_default_my_expenses': 1, 'search_default_no_report': 1},
+            }
